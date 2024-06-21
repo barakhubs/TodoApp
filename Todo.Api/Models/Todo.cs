37efa5a330cd313e780a600890b3a59e1722403d
barakhubs <markbrightbaraka22@gmail.com>
2024-06-21 20:31:36 +0300

    finished setup

---
+++ Todo.Api/Models/Todo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Api.Models
+{
+    public class Todo
+    {
+        
+    }
+}
